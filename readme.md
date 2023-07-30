0303e8fc007d72d560fe4ee8d85264993c9ad78d
Monster <benakevishal2017@gmail.com>
2023-07-30 19:40:25 +0530

    Final Commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# :airplane: Credit Rewards Converter :airplane:
+This is the project repo for the JPMC Software Engineering Lite Program, consult the program instructions for more information.
+
+I completed the JPMorgan Chase & Co - Software Engineering Lite Virtual Experience Program! 💙
+
+During the program, I honed my skills in Java, Git, JUnit, and Unit Testing. I successfully modified an existing system by adding the RewardValue class, enabling conversion between cash and miles. Additionally, I conducted thorough unit testing to ensure accurate conversions. You can find my code on GitHub!
---
+++ src/test/java/RewardValueTests.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RewardValueTests {
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_cash_to_miles() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int expextedMilesValue = (int)(cashValue / MILES_TO_CASH_CONVERSION_RATE);
+        assertEquals(expextedMilesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
+    }
+}
